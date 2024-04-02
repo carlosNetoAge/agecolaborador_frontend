2e34cef64d6acfcce10fb862ce0c7a9d7b0e59f8
Carlos Neto <carlos.neto@agetelecom.com.br>
2024-04-02 13:19:45 -0300

    production

---
+++ dist/assets/HomePage-X12aztzj.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-CC6L5aGG.js";const t={},n={class:"home__container"};function s(_,r){return c(),o("div",n)}const f=e(t,[["render",s]]);export{f as default};
--- services/api.ts
+++ services/api.ts
@@ -2,8 +2,8 @@ import VueAxios from 'axios'
 import Cookie from "js-cookie";
 
 export const AXIOS = VueAxios.create({
-    // baseURL: `https://v2.ageportal.agetelecom.com.br/portal/`,
-    baseURL: `http://localhost:8000/portal/`,
+    baseURL: `https://v2.ageportal.agetelecom.com.br/portal/`,
+    // baseURL: `http://localhost:8000/portal/`,
     //baseURL: `http://v1.portal.tote.local/api`,
     headers: {
         'Authorization': 'Bearer '+Cookie.get('token'),
