6f9d94b19feaa87a572f0cdec7870355319e15b3
Carlos Neto <carlos.neto@agetelecom.com.br>
2024-06-06 16:41:30 -0300

    menu e aniel

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import VueAxios from 'axios';
 import Cookie from 'js-cookie';
 
 export const AXIOS = VueAxios.create({
-    baseURL: "http://localhost:8000/portal/",
+    baseURL: "https://v2.ageportal.agetelecom.com.br/portal/",
     headers: {
         Authorization: "Bearer "+Cookie.get("token"),
     }
