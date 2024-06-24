e2fd60128be809a700c0a569b8da3b42b6139c5c
joao.levyski <joao.levyski@agetelecom.com.br>
2024-06-24 15:38:59 -0300

    arrumar tabela de extratos

--- src/components/app/ageCommission/b2b/financial/audit/cards/ListExtracts.vue
+++ src/components/app/ageCommission/b2b/financial/audit/cards/ListExtracts.vue
@@ -23,7 +23,7 @@ const mock = ref([{
     valor: 700,
   },{
     title: 'teste3',
-    status: 'A ser pago',
+    status: 'Pendente',
     valor: 300,
   }])
 
@@ -36,35 +36,116 @@ const mock = ref([{
         Voltar
       </button>
     </div>
-    <div class="flex justify-center">
-      <div class="overflow-x-auto">
-        <table class="table text-gray-400 border-separate space-y-6 text-2xl">
-        <thead class="bg-age-blue text-white">
-          <tr>
-            <th class="p-3 text center text-3xl">Name</th>
-            <th class="p-3 text center text-3xl">Status</th>
-            <th class="p-3 text center text-3xl">Valeu</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div class="table">
+      <div class="header">
+        <div class="row">
+          <div class="item item1" style="text-align: left;">nome</div>
+          <div class="item" style="text-align: center;">Status</div>
+          <div class="item">valor</div>
+        </div>
+      </div>
+      <div class="body">
+        <div class="row" v-for="(item, index) in mock" :key="index">
+          <div class="item item1" style="text-align: left;">
+            {{ item.title }}
+          </div>
+          <div class="item">
+                <span class="badge"
+                      :class="{
+                  'badge-success' : item.status === 'pago',
+                  'badge-progress' : item.status === 'A ser pago',
+                  'badge-cancel' : item.status === 'Pendente'
+                }">
+                  {{ item.status }}</span>
 
-          
-        </tbody>
-      </table>
+          </div>
+          <div class="item">
+            R$ {{ item.valor }}
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-
+<style scoped lang="scss"> 
 .table {
-    border-spacing: 0 15px;
-    border-radius: 20px;
+  width: 100%;
+  .row {
+    width: 100%;
+    background-color: #fff;
+    border-radius: 5px;
+    @include flex(row, flex-start, center, 1vw);
+    padding: 2vh 1vw;
+    border: 1px solid #cccccc50;
+
+    .item1 {
+      min-width: 60%;
+    }
+
+    .item {
+      width: 20%;
+      text-align: center;
+      font-size: 1.4rem;
+
+      .badge {
+        padding: 0.5vh 1vw;
+        border-radius: 20px;
+        font-size: 1.4rem;
+        font-weight: 500;
+        color: #fff;
+      }
+
+      .badge-success {
+        background-color: #E8FBEF;
+        color: #09D45B;
+      }
+
+      .badge-progress {
+        background-color: #E6F2FE;
+        color: #0A7BF5;
+      }
+
+      .badge-cancel {
+        background-color: #dc354530;
+        color: #dc3545;
+      }
+
+
+
+    }
+
   }
 
-  .table tr {
-    border-radius: 20px;
+
+  .header {
+    margin: 1vh 0;
+    .row {
+      background-color: #F9FCFF;
+
+      .item {
+        font-size: 2rem;
+        font-weight: 600;
+        color: #333;
+      }
+    }
+
   }
-  
+
+  .body {
+    @include flex(column, flex-start, initial, 1vh);
+
+    .row {
+      &:hover {
+        border-color: #cccccc80;
+      }
+      .item {
+        color: #444;
+
+
+      }
+    }
+  }
+}
 </style>
